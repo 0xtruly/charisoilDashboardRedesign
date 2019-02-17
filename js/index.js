b0e6f313ef71bba4203925895abb8267b588371d
Tooyosi <oluwatoyosi.oyegoke@gmail.com>
2019-02-17 20:32:16 +0100

    make links dropdown and add dashbord styling

---
+++ js/index.js
@@ -0,0 +1,26 @@
+var mainUl = document.querySelectorAll(".home");
+
+var subUl = document.querySelectorAll(".home > .submenu > li");
+
+
+// for (let i = 0; i < mainUl.length; i++) {
+//     // var a = mainUl.childElement.length
+//     // alert(a)
+//     console.log(mainUl[i])
+//     mainUl[i].addEventListener("mouseover", function(){
+//         for(let i = 0; i < subUl.length; i++){
+//             if(subUl[i].style.display = "none"){
+//                 subUl[i].style.display = "block";
+//             }
+//         }
+//     })
+//     mainUl[i].addEventListener("mouseout", function(){
+//         for(let i = 0; i < subUl.length; i++){
+//             if(subUl[i].style.display = "block"){
+//                 setTimeout(function(){
+//                     subUl[i].style.display = "none"
+//                 },10000)
+//                 }
+//             }
+//     })    
+// }
