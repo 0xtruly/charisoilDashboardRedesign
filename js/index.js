e9d1d6ca97371d1fe47b0267916d3233415721e0
Tooyosi <oluwatoyosi.oyegoke@gmail.com>
2019-02-25 15:31:41 +0100

    make all report pages responsive

--- js/index.js
+++ js/index.js
@@ -14,15 +14,32 @@
 
 var toggle =  document.getElementById("toggle")
 var dashboard = document.querySelector(".dashboard-nav-left")
+var link = document.querySelectorAll(".dashboard-nav-left ul li a")
 
 toggle.addEventListener("click", function(){
 	if (dashboard.style.opacity == 0) {
-		dashboard.style.opacity = "1"
-		dashboard.style.width = "50%"
-		dashboard.style.height = "100%"
+		dashboard.style.opacity = "1";
+		dashboard.style.width = "220px";
+		dashboard.style.height = "100%";
+		for (let i = 0; i < link.length; i++) {
+			// alert(link[i].textContent)
+			if(link[i].style.display == "inline"){
+				link[i].style.display = "none"
+			}else{
+				link[i].style.display = "inline"
+			}
+		}
 	}else{
-		dashboard.style.opacity = "0"
-		dashboard.style.width = "0%"
-		dashboard.style.height = "0%"
+		dashboard.style.opacity = "0";
+		dashboard.style.width = "0%";
+		dashboard.style.height = "0vh";
+		for (let i = 0; i < link.length; i++) {
+			// alert(link[i].textContent)
+			if(link[i].style.display == "none"){
+				link[i].style.display = "inline"
+			}else{
+				link[i].style.display = "none";
+			}
+		}
 	}
 })
